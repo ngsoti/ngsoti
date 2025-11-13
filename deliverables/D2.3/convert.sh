3016895b07ef60f965182e569d4f2cfc5b096ee4
Paul Jung (Thanat0s) <Paul.Jung@circl.lu>
2025-11-13 21:20:18 +0100

    work on report D2.3

---
+++ deliverables/D2.3/convert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Usage: ./getmd.sh https://your-hedgedoc/note-id
+URL="$1"
+DOCUMENT="document"
+
+# Remove trailing slash AND trailing '#'
+URL="${URL%/}"
+URL="${URL%\#}"
+DL_URL="${URL}/download"
+CONTENT=$(curl -sL "$DL_URL")
+
+if echo "$CONTENT" | grep -qi "<html"; then
+    echo "Error: The note is not public or the server returned HTML."
+        exit 1
+        fi
+        echo "$CONTENT" > $DOCUMENT.md
+        echo "Saved as $DOCUMENT.md"
+pandoc $DOCUMENT.md --columns=10  --pdf-engine=xelatex -V colorlinks=true -V linkcolor=blue  -V urlcolor=red  -V toccolor=gray --number-sections -V toc-own-page=true -V footnotes-pretty=true -V table-use-row-color=true --template eisvogeleuropa  -o ./$DOCUMENT.pdf -F mermaid-filter --toc --lof --data-dir=./eu_template 
+echo "$DOCUMENT.pdf generated"
